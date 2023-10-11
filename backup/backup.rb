bad0b7a952e1b0293c2c118ee876137c7811a7d2
Gerald Bauer <gerald.bauer@gmail.com>
2023-10-11 15:01:43 +0200

    up

--- backup/backup.rb
+++ backup/backup.rb
@@ -3,6 +3,7 @@ require 'gitti/backup'
 
 reposet = GitRepoSet.read( './repos.yml' )
 #=>  560 repo(s) @ 72 org(s)
+#=>  556 repo(s) @ 69 org(s)  - on Oct/11 2023
 
 backup = GitBackup.new( '/backup2023' )
 backup.backup( reposet )
